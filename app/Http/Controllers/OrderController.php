c9e82eff98723d07ff1cacac7096772515802e80
arthur9808 <arturoalvavi98@gmail.com>
2023-04-13 17:13:01 -0600

    centrar pdf loadview

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -127,17 +127,29 @@ class OrderController extends Controller
         $order = Order::where('code', $code)->first();
         $event = Event::where('id', $order->ticket->event_id)->first(); 
         
-        // $data = [
-        //     'event_image'    => $event->image,
-        //     'name_ticket'    => $order->ticket->title,
-        //     'qr'             => $order->svg_qr,
-        //     'website'        => $event->user->web_url
-        // ];
+        $options = ['isPhpEnabled' => true, 'isHtml5ParserEnabled' => true, 'isRemoteEnabled' => true, 'enableFontSubsetting' => true, 'isJavascriptEnabled' => true, 'isPhpEnabled' => true, 'debugKeepTemp' => true, 'isHtml5ParserEnabled' => true, 'chroot' => public_path(), 'defaultMediaType' => 'print', 'enableCssFloat' => true, 'isFontSubsettingEnabled' => true, 'isHtml5ParserEnabled' => true, 'isPhpEnabled' => true, 'isRemoteEnabled' => true, 'enableHtml5Parser' => true, 'enableRemote' => true, 'fontHeightRatio' => 1.3, 'isHtml5ParserEnabled' => true, 'isPhpEnabled' => true, 'logOutputFile' => storage_path('logs/dompdf.log'), 'pdfBackend' => 'CPDF', 'tempDir' => storage_path('app/temp')];
+
         $pdf = PDF::loadView('pages.orders.order-ticket-qr', [
-        'event_image'    => $event->image,
-        'name_ticket'    => $order->ticket->title,
-        'qr'             => $order->svg_qr,
-        'website'        => $event->user->web_url]);
+            'event_image'    => $event->image,
+            'name_ticket'    => $order->ticket->title,
+            'qr'             => $order->svg_qr,
+            'website'        => $event->user->web_url
+        ], [], $options);
+
+        $pdf->setPaper('A4', 'portrait'); // Establecer el tamaño del papel y la orientación
+        $pdf->output(); // Generar el archivo PDF
+
+        // Agregar esta línea para centrar el contenido del PDF
+        $pdf->getDomPDF()->set_option('defaultMediaType', 'print');
+        $pdf->getDomPDF()->set_option('isPhpEnabled', true);
+        $pdf->getDomPDF()->set_option('enable_html5_parser', true);
+        $pdf->getDomPDF()->set_option('chroot', public_path());
+        $pdf->getDomPDF()->set_option('enable_css_float', true);
+        $pdf->getDomPDF()->set_option('font_height_ratio', 1.3);
+        $pdf->getDomPDF()->set_option('pdf_backend', 'CPDF');
+        $pdf->getDomPDF()->set_option('tempDir', storage_path('app/temp'));
+        $pdf->getDomPDF()->set_base_path(public_path());
+        $pdf->getDomPDF()->set_base_path($event->user->web_url);
 
         return $pdf->download('sample.pdf');
     }
