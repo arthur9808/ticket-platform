779c60f1df3704e1782e359e8c10493abb8d3ade
abrahamgarcia27 <abrahamgarcia27@gmail.com>
2023-11-20 10:36:13 -0800

    Currency Change

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -31,7 +31,7 @@ class StripeController extends Controller
             'line_items'  => [
                 [
                     'price_data' => [
-                        'currency'     => 'usd',
+                        'currency'     => 'mxn',
                         'product_data' => [
                             'name' => 'Total',
                         ],
